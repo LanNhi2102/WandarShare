a03d3a93ee4333b1709ac0a01e42c1c8c0a01a90
Lan Nhi <155543658+LanNhi2102@users.noreply.github.com>
2024-02-23 19:42:59 +0700

    update

--- src/components/dashboard/create-listing/CreateList.jsx
+++ src/components/dashboard/create-listing/CreateList.jsx
@@ -29,11 +29,9 @@ const CreateList = () => {
             data-live-search="true"
             data-width="100%"
           >
-            <option data-tokens="type1">Type1</option>
-            <option data-tokens="Type2">Type2</option>
-            <option data-tokens="Type3">Type3</option>
-            <option data-tokens="Type4">Type4</option>
-            <option data-tokens="Type5">Type5</option>
+            <option data-tokens="type1">Exchange</option>
+            <option data-tokens="Type2">Rent</option>
+
           </select>
         </div>
       </div>
@@ -47,11 +45,9 @@ const CreateList = () => {
             data-live-search="true"
             data-width="100%"
           >
-            <option data-tokens="Status1">Status1</option>
-            <option data-tokens="Status2">Status2</option>
-            <option data-tokens="Status3">Status3</option>
-            <option data-tokens="Status4">Status4</option>
-            <option data-tokens="Status5">Status5</option>
+            <option data-tokens="Status1">Old</option>
+            <option data-tokens="Status2">Recent</option>
+
           </select>
         </div>
       </div>
