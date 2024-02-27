a4d45e8776e5d98512b51fea86edef8965404573
Lan Nhi <155543658+LanNhi2102@users.noreply.github.com>
2024-02-27 22:50:48 +0700

    update

--- src/components/register/Form.jsx
+++ src/components/register/Form.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Modal, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+
 
 
 const Form = () => {
