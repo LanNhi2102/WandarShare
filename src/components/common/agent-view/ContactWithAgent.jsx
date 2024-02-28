1d6e0c50f3ad229be3dff2960762cfa1a694bd57
Le Nguyen Minh Huy <huylnmse161054@fpt.edu.vn>
2024-02-28 13:20:18 +0700

    Update ContactWithAgent.jsx

--- src/components/common/agent-view/ContactWithAgent.jsx
+++ src/components/common/agent-view/ContactWithAgent.jsx
@@ -1,59 +1,21 @@
+import { Calendar } from "react-check-in-out-calendar";
+
 const ContactWithAgent = () => {
   return (
     <form action="#">
       <ul className="sasw_list mb0">
-        <li className="search_area">
-          <div className="form-group mb-3">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Your Name"
-              required
-            />
-          </div>
-        </li>
         {/* End li */}
         <li className="search_area">
           <div className="form-group mb-3">
-            <input
-              type="number"
-              className="form-control"
-              placeholder="Phone"
-              required
-            />
           </div>
-        </li>{" "}
-        {/* End li */}
-        <li className="search_area">
-          <div className="form-group mb-3">
-            <input
-              type="email"
-              className="form-control"
-              placeholder="Email"
-              required
-            />
-          </div>
-        </li>{" "}
-        {/* End li */}
-        <li className="search_area">
-          <div className="form-group mb-3">
-            <textarea
-              id="form_message"
-              name="form_message"
-              className="form-control "
-              rows="1"
-              required
-              placeholder="Choose Days"
-              
-            ></textarea>
-          </div>
+          <CalendarComponent/>
         </li>{" "}
         {/* End li */}
        
         <li>
           <div className="search_option_button">
             <button type="submit" className="btn btn-block btn-thm w-100">
-              Search
+              Book
             </button>
           </div>
         </li>{" "}
