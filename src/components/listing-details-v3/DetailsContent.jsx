2f9023ebd87580c97b061720570e4aa34d945085
Le Nguyen Minh Huy <huylnmse161054@fpt.edu.vn>
2024-03-01 21:46:38 +0700

    Update DetailsContent.jsx

--- src/components/listing-details-v3/DetailsContent.jsx
+++ src/components/listing-details-v3/DetailsContent.jsx
@@ -1,17 +1,7 @@
-import Comments from "../blog-details/Comments";
-import Ratings from "../blog-details/Ratings";
-import ReviewBox from "../blog-details/ReviewBox";
 import AdditionalDetails from "../common/listing-details/AdditionalDetails";
-import Attachments from "../common/listing-details/Attachments";
-import FloorPlans from "../common/listing-details/FloorPlans";
 import PropertyDescriptions from "../common/listing-details/PropertyDescriptions";
 import PropertyDetails from "../common/listing-details/PropertyDetails";
-import PropertyFeatures from "../common/listing-details/PropertyFeatures";
-import PropertyItem from "../common/listing-details/PropertyItem";
-import PropertyLocation from "../common/listing-details/PropertyLocation";
-import PropertyVideo from "../common/listing-details/PropertyVideo";
-import WalkScore from "../common/listing-details/WalkScore";
-import WhatsNearby from "../common/listing-details/WhatsNearby";
+
 
 const DetailsContent = () => {
   return (
