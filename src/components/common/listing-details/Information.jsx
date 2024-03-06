f4694a392249ff80d688849c6408827fe2c3dc81
Le Nguyen Minh Huy <huylnmse161054@fpt.edu.vn>
2024-03-06 08:50:32 +0700

    Create Information.jsx

---
+++ src/components/common/listing-details/Information.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+
+function Payment() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    address: '',
+    city: '',
+    country: '',
+    zip: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Xử lý dữ liệu đầu vào ở đây, ví dụ: gửi dữ liệu đến máy chủ
+    console.log(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        First Name:
+        <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+        Last Name:
+        <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input type="email" name="email" value={formData.email} onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+        Address:
+        <input type="text" name="address" value={formData.address} onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+        City:
+        <input type="text" name="city" value={formData.city} onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+        Country:
+        <input type="text" name="country" value={formData.country} onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+        Zip:
+        <input type="text" name="zip" value={formData.zip} onChange={handleChange} />
+      </label>
+    </form>
+  );
+}
+
+export default Payment;
